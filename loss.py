4e87764e58a13d282098ca0a5f0990c3cbc9f436
AnyangTong <121151293+tonganyang@users.noreply.github.com>
2023-03-25 17:30:04 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -18,24 +18,26 @@ def supervised_loss(logits, targets, use_hard_labels=True, reduction='none'):
         return nll_loss
 
 def unsupervised_loss(logits1, logits2, p_cutoff):
-    logits2 = logits2.detach()
-    pseudo_label = torch.softmax(logits2, dim=-1)
-    max_probs, max_idx = torch.max(pseudo_label, dim=-1)
-
-    mask = max_probs.ge(p_cutoff).float()
     
-    select = max_probs.ge(p_cutoff).long()
-
+    mask = [1 for _ in range(len(logits2))]
+    
+    for i in range(len(logits2)):
+        
+        logits = logits2[i].detach()
+        pseudo_label = torch.softmax(logits, dim=-1)
+        max_probs, max_idx = torch.max(pseudo_label, dim=-1)
+        mas = max_probs.ge(p_cutoff).float()
+        mask = map(lambda x,y:x*y, mask, mas)
+    
     return (supervised_loss(logits1, max_idx.detach(), use_hard_labels=True,
-                    reduction='none') * mask).mean(), select, max_idx.long()
+                    reduction='none') * mask).mean()
 
 def uncertainty_loss(logit_S):
     # the Jensen-Shannon divergence between p(x) and mean
-    length = len(logit_S)
     S = nn.Softmax(dim=1)
-    mean = sum([S(i) for i in logit_S]) / length
+    mean = sum([S(i) for i in logit_S]) / len(logit_S)
     kl_divergence = nn.KLDivLoss()
-    JSloss = sum([kl_divergence(i.log(), mean) for i in logit_S]) / length
+    JSloss = sum([kl_divergence(i.log(), mean) for i in logit_S]) / len(logit_S)
     
     return JSloss
 
@@ -49,6 +51,7 @@ class ContrastiveLoss(nn.Module):
     def forward(self, emb_i, emb_j):
         z_i = F.normalize(emb_i, dim=1)
         z_j = F.normalize(emb_j, dim=1)
+
         representations = torch.cat([z_i, z_j], dim=0)
         similarity_matrix = F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)
         
